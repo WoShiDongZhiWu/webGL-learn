69d6e28beaef2d2fe8087276061d6dd3578e8858
wudong <915001258@qq.com>
2017-11-08 22:19:59 +0800

    绘制点，鼠标点击绘制

--- 05-ClickedPoints/ClickedPoints.js
+++ 05-ClickedPoints/ClickedPoints.js
@@ -25,5 +25,28 @@ function main(){
         return;
     }
     //初始化着色器
-    
+    if(!initShaders(gl,VSHADER_SOURCE,FSHADER_SOURCE)){
+        console.log("error");
+        return;
+    }
+    //获取attribute变量的存储位置
+    var a_Position = gl.getAttribLocation(gl.program,'a_Position');
+    if(a_Position< 0 ){
+        console.log("error");
+        return;
+    }
+    //注册鼠标点击事件响应函数
+    canvas.onmousedown = function(ev){click(ev,gl,canvas,a_Position);};
+    //设置背景色
+    gl.clearColor(0.0,0.0,0.0,1.0);
+    //清除canvas
+    gl.clear(gl.COLOR_BUFFER_BIT);
+}
+
+var g_points = [];//鼠标点击位置的数组
+function click(ev,gl,VSHADER_SOURCE,FSHADER_SOURCE){
+    var x = ev.clientX;//鼠标点击处的x坐标
+    var y = ev.clientY;//鼠标点击处的y坐标
+    var rect = ev.target.getBoundingClientRect();//获取canvas的边界坐标
+    x = ((x - rect.left))
 }
