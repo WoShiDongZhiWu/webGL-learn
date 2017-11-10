99b377f021477620e4cb9c40bf7beee757d86dc4
wudong <915001258@qq.com>
2017-11-10 14:25:54 +0800

    绘制点，鼠标点击绘制

--- 05-ClickedPoints/ClickedPoints.js
+++ 05-ClickedPoints/ClickedPoints.js
@@ -48,5 +48,20 @@ function click(ev,gl,VSHADER_SOURCE,FSHADER_SOURCE){
     var x = ev.clientX;//鼠标点击处的x坐标
     var y = ev.clientY;//鼠标点击处的y坐标
     var rect = ev.target.getBoundingClientRect();//获取canvas的边界坐标
-    x = ((x - rect.left))
+    x = ((x - rect.left) - canvas.height/2)/(canvas.height/2);
+    y = (canvas.width/2 - (y - rect.top))/(canvas.width/2);
+    //将坐标存储到g_points数组中
+    g_points.push(x);g_points.push(y);
+
+    //清除canvas
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    var len = g_points.length;
+    for(var i = 0;i < len; i += 2){
+        //将点的位置传输到a_Position变量中
+        gl.vertexAttrib3f(a_Position,g_points[i],g_points[i+1],0.0);
+
+        //绘制点
+        gl.drawArrays(gl.POINTS,0,1);
+    }
 }
