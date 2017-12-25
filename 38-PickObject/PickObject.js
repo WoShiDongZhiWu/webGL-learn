54c2b54dbac7ee05b5fd7d7dbce581859841a951
wudong <915001258@qq.com>
2017-12-25 21:01:34 +0800

    选中立方体并弹框

--- 38-PickObject/PickObject.js
+++ 38-PickObject/PickObject.js
@@ -4,11 +4,11 @@ var VSHADER_SOURCE =
 'attribute vec4 a_Position;\n' +
 'attribute vec4 a_Color;\n' +
 'uniform mat4 u_MvpMatrix;\n' +
-'uniform bool u_Clicked;\n' + // Mouse is pressed
+'uniform bool u_Clicked;\n' + // 按下鼠标
 'varying vec4 v_Color;\n' +
 'void main() {\n' +
 '  gl_Position = u_MvpMatrix * a_Position;\n' +
-'  if (u_Clicked) {\n' + //  Draw in red if mouse is pressed
+'  if (u_Clicked) {\n' + // 鼠标按下时用红色进行绘制
 '    v_Color = vec4(1.0, 0.0, 0.0, 1.0);\n' +
 '  } else {\n' +
 '    v_Color = a_Color;\n' +
